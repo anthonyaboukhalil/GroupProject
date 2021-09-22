87d538417ad3ea5c585843558089b23a7c427a73
Tim <mammadov_teymur@yahoo.com>
2021-09-22 14:46:22 -0700

    Added sum of odd numbers

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -7,13 +7,17 @@ public final class App {
  
     public static void main(String[] args) {
         int n  = 50 ;
-        int sum = 0;
+        int sum = 0, sum_odd = 0;
         for (int i = 0; i < n; i++) {
             if(i%2==0)
                 sum+= i;
+            if(i%2==1)
+                sum_odd += i;
 
         }
         System.out.println("The sum of all even integers is: " + sum);
+
+        System.out.println("The sum of all odd integers is: " + sum_odd);
     
     }
 }
