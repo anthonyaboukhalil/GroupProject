36bb146d0c75d85e02a9a19e901dabde5eb16c56
DustyWizard <36353123+DustyWizard@users.noreply.github.com>
2021-09-23 13:57:06 -0700

    Added codebase for the userInput function.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,9 @@
 package com.example;
 
+import java.util.Scanner;
+
 public  class App {
-    
+
     public static  double evenAvg(int n, int sum){
         int average = sum/n;
         return average;
@@ -11,14 +13,27 @@ public  class App {
     public static double oddAvg(int n, int sum){
         return (double) sum/n;
     }
- 
- 
- 
+
+    public static int userInput(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a positive integer ( (-) signs will be removed): ");
+        while(!scanner.hasNextInt()){
+            scanner.next();
+            System.out.println("Please Enter a Valid Integer!\n");
+            System.out.println("Enter a positive integer ( (-) signs will be removed): ");
+        }
+        int x = scanner.nextInt();
+        scanner.close();
+        return Math.abs(x);
+    }
+
+
+
     public static void main(String[] args) {
-        int n =50 ;
+        int n =userInput();
         int counter=0;
         int sum = 0, sum_odd =0, count_odd = 0;
-        
+
 
         for (int i = 0; i < n; i++) {
             if(i%2==0){
@@ -30,10 +45,10 @@ public  class App {
             }
 
 
-                
+
             }
 
-        
+
 
 
         System.out.println("The sum of all even integers is: " + sum);
@@ -43,7 +58,7 @@ public  class App {
         System.out.println("Average of all odd integers is: " + oddAvg(count_odd, sum_odd));
 
         System.out.println("The average of all even integers is: " + evenAvg(counter,sum));
-    
+
 
     }
 }
